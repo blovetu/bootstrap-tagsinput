b9e80033e55eea0373b949a833f99fca35f1e75b
YangFei <yf900825@163.com>
2016-04-08 13:35:58 +0800

    add custom config options to customize container div's style

--- src/bootstrap-tagsinput.js
+++ src/bootstrap-tagsinput.js
@@ -46,8 +46,11 @@
     this.objectItems = options && options.itemValue;
     this.placeholderText = element.hasAttribute('placeholder') ? this.$element.attr('placeholder') : '';
     this.inputSize = Math.max(1, this.placeholderText.length);
-
-    this.$container = $('<div class="bootstrap-tagsinput"></div>');
+    var containerClass = "";
+    if(options.containerClass){
+      containerClass = " " + options.containerClass;
+    }
+    this.$container = $('<div class="bootstrap-tagsinput"'+containerClass+'></div>');
     this.$input = $('<input type="text" placeholder="' + this.placeholderText + '"/>').appendTo(this.$container);
 
     this.$element.before(this.$container);
